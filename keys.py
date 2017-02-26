fb342d29604eaa67f2b0ecec2c66662278e7347a
aesquis <aesquis@gmail.com>
2017-02-26 15:25:59 +0100

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,6 @@
+# Claus generades a veveloper
+keys = dict(
+consumer_key = 'Xxxxxxxxxxxxxxxxxxxxxxxxx',
+consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxx',
+access_token = 'xxxxxxxxxxxxxxxxxxxxxxxx',
+access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxxx',
